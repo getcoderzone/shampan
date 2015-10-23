6cf1e2fac04eeb72eaca32173bf525b8a758e68a
GetCoderZone <getcoderzone@gmail.com>
2015-10-24 04:40:28 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,5 @@
 <?php 
-<<<<<<< HEAD
 	echo "Hello Shafiqul";
+	echo "Hello shopon";
 ?>
-=======
-	echo "Hello Worldgfgfgf";
-	ehco "hello shopon";
-?>
->>>>>>> d5a0dca51ba858392d7f9565bc9ef69be69b88ea
+
